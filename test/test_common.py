8af60b864b68d5be79d398882d1a21e5b46640e2
yayakona <hirose_kazuto@yahoo.co.jp>
2018-11-21 01:30:48 +0900

    パッケージ化とテストクラス追加

---
+++ test/test_common.py
@@ -0,0 +1,17 @@
+# coding: UTF-8
+
+import unittest
+from common import common
+
+
+class TestCommon(unittest.TestCase):
+
+
+    def test_get_type_name(self):
+       self.assertEqual(common.get_type_name(1), 'int')
+       self.assertEqual(common.get_type_name("aa"), 'string')
+       self.assertEqual(common.get_type_name([1,2]), 'list')
+
+
+if __name__ == "__main__":
+    unittest.main()

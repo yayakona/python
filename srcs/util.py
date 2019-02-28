32a0f5dccb018ca6b6558681d2787076cc9bc9b6
yayakona <44542602+yayakona@users.noreply.github.com>
2019-02-28 20:07:59 +0900

    Update util.py print_ast追加 未テスト

--- srcs/util.py
+++ srcs/util.py
@@ -41,6 +41,26 @@ def fizz_buzz(stop=100):
     for i range(1,stop):
         print((("Fizz" if not i%3)+("Buzz") if not i%5) or i)
 
+"""
+input=3
+======
+*
+**
+***
+======
+
+input=5
+======
+*
+**
+***
+****
+*****
+======
+"""
+def print_ast(cnt):
+    for i in range(cnt):
+        print("*"*i)
 
 if __name__ == "__main__":
     for i in get_pair_iter(iter([1,2,3,4,5])):

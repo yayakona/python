cc4e05a414194882ee76d6e5c4ed75886f71df1e
yayakona <44542602+yayakona@users.noreply.github.com>
2019-02-28 19:50:10 +0900

    Update util.py フィズバズ追加 未テスト

--- srcs/util.py
+++ srcs/util.py
@@ -37,6 +37,10 @@ def get_yyy_format_title(title: str, N: int=75) -> str:
 
     edit_str += "*" * 60
 
+def fizz_buzz(stop=100):
+    for i range(1,stop):
+        print((("Fizz" if not i%3)+("Buzz") if not i%5) or i)
+
 
 if __name__ == "__main__":
     for i in get_pair_iter(iter([1,2,3,4,5])):

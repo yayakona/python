aae841fa94adc0804f71decfeaca2261446e66ce
yayakona <44542602+yayakona@users.noreply.github.com>
2019-03-01 09:36:48 +0900

    Update load_data.py

--- stocks/Stocks/load/load_data.py
+++ stocks/Stocks/load/load_data.py
@@ -8,7 +8,10 @@ def load_data(stock_id: Int, start: String = None, end: String = None):
     Parameter
     ___
         stock_id
-
+        start
+        end
+        all_update
+        diff_update
 
 
 
@@ -18,6 +21,14 @@ def load_data(stock_id: Int, start: String = None, end: String = None):
     now_date: String = datetime.datetime.now().strftime("%Y/%m/%d")
     # DBに最後にアップデートした取得した日付をYYYYMMDD形式で取得する
     updated_date: String = "TODO"
-    
 
-    ## DBからstock_idの
+    conn = db.cor
+    
+    if all_update_flag:
+        conn.executor(Sqls.all_delete(stock_id))
+download_data(stock_id)
+    
+    elif diff_update_flag or update_date < now_date:
+    
+    
+        

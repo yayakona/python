2b112b88c213274aeef280e807e2d5d3b08128d6
yayakona <44542602+yayakona@users.noreply.github.com>
2019-03-08 09:20:26 +0900

    Update UsesIO.py print_2d_list first commit

--- Utils/Untils/UsesIO.py
+++ Utils/Untils/UsesIO.py
@@ -4,3 +4,8 @@ from typing import List, Set, Type, Any, Iterable
 
 def input_line(type_: Type = str):
     return list(map(type_, input().split()))
+
+
+def print_2d_list(lst2):
+    "¥n".join(["".join(str(v) for v in lst) for lst in lst2])
+

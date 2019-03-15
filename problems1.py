fa197fd100030bf18f01ce0b35e2076dd26b10d3
yayakona <44542602+yayakona@users.noreply.github.com>
2019-03-15 20:13:47 +0900

    Create problems1.py

---
+++ problems1.py
@@ -0,0 +1,24 @@
+
+def p1():
+    """
+    a+b=5
+    """
+    a = 3
+    b = 2
+
+    #
+    result = 
+
+    #
+
+    print("a+b={}".format(result))
+
+def p2():
+    """
+    1+2+3...+10=55
+    """
+    result = 0
+    for i in range(1,11):
+        #
+        
+        #
